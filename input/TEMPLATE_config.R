df075cd6756a156a2a931dd86a1b4ac35ce45a16
Agnes B <gnessou91@gmail.com>
2020-05-28 17:40:12 +0200

    Add templates of input files

---
+++ input/TEMPLATE_config.R
@@ -0,0 +1,11 @@
+list_transfo = "input/list_transfo.tsv"
+bool_phase_2 = FALSE
+lib_perso = ""  # give the personnal R library if needed
+nb_transformation = 3
+cores = NA  # if NA cores = number of available cores -1
+min_peak_intensity = 5e5
+mz_ppm = 10  # confidence m/z to search chromatogram
+rt_windows = 5  # windows size to consider a peak in chromatogram
+nb_scan = 2  # minimum number of scans in chromatogram peaks
+wdw_mz_ms2 = 1  # total window width
+minimum_mz = 200 # Minimum M/Z value of a metabolite to be detected
