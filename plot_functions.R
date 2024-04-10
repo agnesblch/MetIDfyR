c238e8cf1f30a31235ba71c2eb2a9651cae6a4bf
Agnes Barnabe <a.barnabe@lchfrance.fr>
2024-04-10 09:28:31 +0000

    Edit output graphs

--- plot_functions.R
+++ plot_functions.R
@@ -19,7 +19,7 @@ doChromato = function(chrom_dataframe, line_size = 0.5, alpha = 0.4){
     geom_text(aes(label = ifelse(index > 0, index, '')),
               hjust = -0.2, vjust= 1, col="black", size=3) +
     theme_classic() +
-    guides(alpha = FALSE) +  #don't show legend for alpha
+    guides(alpha = "none") +  #don't show legend for alpha
     scale_y_continuous(expand=c(0,0),
                        labels = function(x) format(x, scientific = TRUE)) +
     scale_color_manual(values = myColors,
@@ -29,14 +29,14 @@ doChromato = function(chrom_dataframe, line_size = 0.5, alpha = 0.4){
     theme(legend.position = "bottom",
           legend.margin = margin(0,0,0,0),
           legend.box.margin = margin(-20,-10,0,-10),
-          axis.text=element_text(size = 6, colour="black"),
+          axis.text=element_text(size = 8, colour="black"),
+          axis.title = element_text(size=8),
           axis.ticks = element_line(colour="black"),
-          plot.subtitle = element_text(size = 6), 
-          plot.title = element_text(size = 7),
-          legend.text = element_text(size=6),
-          legend.title = element_text(size=6),
-          axis.title = element_text(size=6),
-          strip.text = element_text(size=6)) 
+          plot.subtitle = element_text(size = 8), 
+          plot.title = element_text(size = 9),
+          legend.text = element_text(size=8),
+          legend.title = element_text(size=8),
+          strip.text = element_text(size=8)) 
   
   return(gpl)
   
@@ -58,7 +58,7 @@ doMassSpectrum = function(data_ms, polarity = NULL){
   ggp = ggplot(data=abund_exp, aes(mz, perctot)) +
     geom_segment(data=abund_th, aes(xend=mz, yend=0, col = "red", alpha=0.8, linetype="Theoric"), size=2) +
     geom_segment(aes(xend=mz, yend=0, linetype = "Experimental"), col = "blue", alpha = 0.8) +
-    theme_classic() + guides(alpha = F, colour  = F, size=F) +
+    theme_classic() + guides(alpha = "none", colour  = "none", size="none") +
     geom_label_repel(
       aes(mz, perctot, label = ifelse(!is.na(exp_ppm), 
                                       paste0(round(mz,5), "\n(", sprintf("%+g", round(exp_ppm, 3)), " ppm)"), '')),
@@ -73,10 +73,10 @@ doMassSpectrum = function(data_ms, polarity = NULL){
                           guide = guide_legend(override.aes = list(color=c("blue", "red")))) +
     scale_x_continuous(limits = c(abund_th$mz[1]-1.5, abund_th$mz[1]+4.5 )) +
     coord_cartesian(ylim=c(0, 1)) +
-    theme(plot.subtitle = element_text(size=6),
-          axis.text = element_text(size=6, colour="black"),
-          legend.text = element_text(size=6), 
-          plot.title = element_text(size = 7),
+    theme(plot.subtitle = element_text(size=8),
+          axis.text = element_text(size=8, colour="black"),
+          legend.text = element_text(size=8), 
+          plot.title = element_text(size = 9),
           legend.position = "bottom",
           legend.margin = margin(0,0,0,0),
           legend.box.margin = margin(-20,-10,0,-10),
@@ -118,23 +118,39 @@ doMS2 = function(data_ms2, exists_ref = T){
         geom_text(data=data_ms2 %>% filter(type=="exp"), aes(label=index_exp),
                   hjust = -0.5, col="black", size=2) +
         theme(plot.margin = unit(c(1,1,0,1), "lines"), 
-              axis.text = element_text(size=6,colour="black"), 
-              axis.title = element_text(size = 6), 
-              plot.title = element_text(size = 7),
-              axis.ticks= element_line(colour="black")) +
+              axis.text = element_text(size=8,colour="black"), 
+              axis.title = element_text(size = 8), 
+              plot.title = element_text(size = 9),
+              axis.ticks= element_line(colour="black"), 
+              legend.position = "none") +
         xlab("m/z") + ylab("Relative abundance (%)") + ggtitle("MS2 reference (A) versus metabolite (B)") + 
-        annotate("text", x = max(data_ms2$mz)+5, y = c(1, -1), label = c("A", "B"), size = 4)
+        annotate("text", x = max(data_ms2$mz)+5, y = c(1, -1), label = c("A", "B"), size = 4) +
+        geom_label_repel(
+          aes(mz, intensity, label = ifelse(type=="exp"|type=="precursor_exp", round(mz, 5), "")),
+          size = 3,
+          point.padding = unit(0.1, "lines"),
+          box.padding = unit(1, "lines"),
+          segment.color = 'grey50'
+        )
       
     }else{
       ggplot(as.data.frame(cbind(data_ms2@mz, data_ms2@intensity)), aes(data_ms2@mz, data_ms2@intensity)) +
-        theme_classic() + theme(axis.text = element_text(size=6,colour="black"), 
-                                axis.title = element_text(size = 6), 
-                                plot.title = element_text(size = 7),
-                                axis.ticks=element_line(colour="black")) +
+        theme_classic() + theme(axis.text = element_text(size=8,colour="black"), 
+                                axis.title = element_text(size = 8), 
+                                plot.title = element_text(size = 9),
+                                axis.ticks=element_line(colour="black"), 
+                                legend.position = "none") +
         geom_segment(aes(xend=data_ms2@mz, yend=0), col="#F46D43") +
         geom_point(col="#F46D43") +
         scale_y_continuous(expand=c(0,0)) +
-        ggtitle("Metabolite experimental MS2") + xlab("m/z") + ylab("Relative abundance (%)")
+        ggtitle("Metabolite experimental MS2") + xlab("m/z") + ylab("Relative abundance (%)") +
+        geom_label_repel(
+          aes(data_ms2@mz, data_ms2@intensity, label = round(data_ms2@mz, 5)),
+          size = 3,
+          point.padding = unit(0.1, "lines"),
+          box.padding = unit(1, "lines"),
+          segment.color = 'grey50'
+        )
     }
   }else{
     return(NULL)
